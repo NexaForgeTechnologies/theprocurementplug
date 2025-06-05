ba081f4dd99f4c6904d892b3a871b9a8796b53ea
AhmadFiaz-1 <h.ahmad.fiaz@gmail.com>
2025-06-05 23:03:42 +0500

    added qr code for our page

---
+++ src/app/qr-code/page.js
@@ -0,0 +1,17 @@
+"use client"
+
+// pages/qr-event.js
+import { QRCodeCanvas } from 'qrcode.react';
+
+export default function QREventPage() {
+  const url = 'https://www.theprocurementplug.com/event/elevate2025-manchester';
+
+  return (
+    // <div className="flex flex-col items-center justify-center min-h-screen p-8 text-[#363636]">
+    //   <h1 className="text-2xl font-bold mb-4">Scan to Visit Manchester Event</h1>
+    //   <QRCodeCanvas value={url} size={256} />
+    //   <p className="mt-4 text-sm">{url}</p>
+    // </div>
+    <></>
+  );
+}
