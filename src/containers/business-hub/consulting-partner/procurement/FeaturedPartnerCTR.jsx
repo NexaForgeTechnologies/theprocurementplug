c6f690359c0008a61b5f375d59ec825fa31226d5
MAbdullahkhalilch <ak24566316@gmail.com>
2025-06-05 19:58:14 +0500

    build issue resolve

--- src/containers/business-hub/consulting-partner/procurement/FeaturedPartnerCTR.jsx
+++ src/containers/business-hub/consulting-partner/procurement/FeaturedPartnerCTR.jsx
@@ -3,14 +3,103 @@
 import React, { useState } from "react";
 
 import ConsultantTile from "@/components/business-hub/consultinng-partner/esg-sustainability/ConsultantTile";
-import { consultantPartners } from "@/app/business-hub/consulting-partner/esg-sustainability/data";
-
 
 function ConsultantPartnerCTR() {
 
   const [selectedIndustry, setSelectedIndustry] = useState("");
   const [selectedRegion, setSelectedRegion] = useState("");
   const [visibleCount, setVisibleCount] = useState(4);
+
+  const consultantPartners = [
+    {
+      id: 1,
+      img: "/images/guess/guess-1.jpg",
+      heading: "Oliver Jones",
+      text1: "Consulting Director",
+      industry_id: 1,
+      industry_name: "Industry One",
+      region_id: 1,
+      region_name: "Region One",
+      text2: "H&Z Management Consulting",
+    },
+    {
+      id: 2,
+      img: "/images/guess/guess-2.jpg",
+      heading: "Kiran Kachela",
+      text1: "CEO",
+      industry_id: 2,
+      industry_name: "Industry Two",
+      region_id: 2,
+      region_name: "Region Two",
+      text2: "CI Projects",
+    },
+    {
+      id: 3,
+      img: "/images/guess/guess-3.jpg",
+      heading: "Sarah Socca",
+      text1: "CEO",
+      industry_id: 3,
+      industry_name: "Industry Three",
+      region_id: 3,
+      region_name: "Region Three",
+      text2: "Co Comply",
+    },
+    {
+      id: 4,
+      img: "/images/guess/guess-1.jpg",
+      heading: "Oliver Jones",
+      text1: "Consulting Director",
+      industry_id: 1,
+      industry_name: "Industry One",
+      region_id: 1,
+      region_name: "Region One",
+      text2: "H&Z Management Consulting",
+    },
+    {
+      id: 5,
+      img: "/images/guess/guess-2.jpg",
+      heading: "Kiran Kachela",
+      text1: "CEO",
+      industry_id: 2,
+      industry_name: "Industry Two",
+      region_id: 2,
+      region_name: "Region Two",
+      text2: "CI Projects",
+    },
+    {
+      id: 6,
+      img: "/images/guess/guess-1.jpg",
+      heading: "Oliver Jones",
+      text1: "Consulting Director",
+      industry_id: 1,
+      industry_name: "Industry One",
+      region_id: 1,
+      region_name: "Region One",
+      text2: "H&Z Management Consulting",
+    },
+    {
+      id: 7,
+      img: "/images/guess/guess-3.jpg",
+      heading: "Sarah Socca",
+      text1: "CEO",
+      industry_id: 3,
+      industry_name: "Industry Three",
+      region_id: 3,
+      region_name: "Region Three",
+      text2: "Co Comply",
+    },
+    {
+      id: 8,
+      img: "/images/guess/guess-1.jpg",
+      heading: "Oliver Jones",
+      text1: "Consulting Director",
+      industry_id: 1,
+      industry_name: "Industry One",
+      region_id: 1,
+      region_name: "Region One",
+      text2: "H&Z Management Consulting",
+    },
+  ];
 
   const filteredConsultants = consultantPartners.filter((partner) => {
     const matchesIndustry = selectedIndustry
