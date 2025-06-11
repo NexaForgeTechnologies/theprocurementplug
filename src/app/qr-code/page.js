c7c0ad46c2c0e2f8df6abaac12ec18f7e79881cd
AhmadFiaz-1 <h.ahmad.fiaz@gmail.com>
2025-06-11 19:56:14 +0500

    update text qr

--- src/app/qr-code/page.js
+++ src/app/qr-code/page.js
@@ -8,7 +8,7 @@ export default function QREventPage() {
 
   return (
     // <div className="flex flex-col items-center justify-center min-h-screen p-8 text-[#363636]">
-    //   <h1 className="text-2xl font-bold mb-4">Scan to Visit Manchester Event</h1>
+    //   <h1 className="text-2xl font-bold mb-4">Scan to access Manchester Elevate 2025 Agenda</h1>
     //   <QRCodeCanvas value={url} size={256} />
     //   <p className="mt-4 text-sm">{url}</p>
     // </div>
