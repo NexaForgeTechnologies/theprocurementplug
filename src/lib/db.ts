2d5fe7b1cb47d260bb76ff3528f79d32827f7d6e
AhmadFiaz-1 <h.ahmad.fiaz@gmail.com>
2025-09-08 14:45:54 +0500

    fixed database configurations

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -4,9 +4,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createPool({
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),  // ✅ Convert string → number
-  user: process.env.DATABASE_USER,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),  // ✅ Convert string → number
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
