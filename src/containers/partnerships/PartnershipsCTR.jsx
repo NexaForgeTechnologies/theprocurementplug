d3e09fae333321f8dd6a3d124677c77ad707ac1c
NexaForgeTechnologies <project@nexaforgetech.com>
2025-09-15 14:58:46 +0500

    Update PartnershipsCTR.jsx

--- src/containers/partnerships/PartnershipsCTR.jsx
+++ src/containers/partnerships/PartnershipsCTR.jsx
@@ -11,7 +11,7 @@ function PartnershipsCTR() {
     {
       id: 1,
       title: 1,
-      heading: "1Targeted Market Exposure",
+      heading: "Targeted Market Exposure",
       text: "Showcase your brand, services, and products to procurement professionals actively searching for industry solutions.",
     },
     {
