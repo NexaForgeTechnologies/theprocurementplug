bc95fe9759b63e25b1e09abb6d4c73caecdcc241
AhmadFiaz-1 <h.ahmad.fiaz@gmail.com>
2025-11-03 16:03:39 +0500

    fixed event speakers color and role

--- src/components/events/GuestComp.jsx
+++ src/components/events/GuestComp.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import React from "react";
 
 function GuessComp({ data }) {
+  console.log(data);
+
   return (
     <>
       <div
-        style={{ backgroundColor: data.bg_color ? data.bg_color : "#faf8f5" }}
+        style={{ backgroundColor: data.role != "" ? data.bg_color : "#faf8f5" }}
         className={`${
-          data.bg_color ? "text-white" : "text-[#363636]"
+          data.role != "" ? "text-white" : "text-[#363636]"
         }  border border-[#b08d57]  px-4 py-10 rounded-xl w-full flex flex-col items-center justify-center gap-2 text-center`}
       >
         <Image
@@ -18,13 +20,15 @@ function GuessComp({ data }) {
           height={130}
         />
         <h2 className="text-xl md:text-2xl font-extrabold">{data.name}</h2>
-        <h3
-          className={`${
-            data.bg_color ? "text-white" : "text-[#b08d57]"
-          } text-xl md:text-2xl font-medium`}
-        >
-          {data.role}
-        </h3>
+        {data.role && (
+          <h3
+            className={`${
+              data.role != "" ? "text-white" : "text-[#b08d57]"
+            } text-xl md:text-2xl font-medium`}
+          >
+            {data.role == "1" ? "Host" : "Voice of the Day"}
+          </h3>
+        )}
         <div className="mt-2 flex flex-col items-center">
           <span className="font-bold">{data.designation}</span>
           <span className="font-bold">{data.company}</span>
