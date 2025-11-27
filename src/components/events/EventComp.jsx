4e72f7b483190fffbc5edc9cb3286f70f01e53fe
AhmadFiaz-1 <h.ahmad.fiaz@gmail.com>
2025-11-28 01:41:27 +0500

    fixed tag name

--- src/components/events/EventComp.jsx
+++ src/components/events/EventComp.jsx
@@ -16,14 +16,13 @@ function EventComp({ data, url }) {
       className="w-full border border-[#D09B48] transition-all duration-200 ease-in-out transform hover:shadow-2xl hover:border-[#A020F0] bg-white rounded-[6px]"
     >
       <div className="p-5">
-        {data?.event_name && (
+        {data?.event_tag_name && (
           <div className="inline-flex">
             <p
               className="mb-2 text-[12px] rounded-[2px] px-2 py-1 text-[#FFFFFF]"
               style={{ backgroundColor: "#85009D" || "#85009D" }}
             >
-              {/* {data?.event_name} */}
-              Tag
+              {data?.event_tag_name}
             </p>
           </div>
         )}
